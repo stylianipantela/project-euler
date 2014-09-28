7d71f641162a25e18f17565e84f7ad9f81d84f6f
Stella Pantela <stella.pantela@gmail.com>
2014-09-28 10:06:33 -0400

    problem1 improved

--- problems.cpp
+++ problems.cpp
@@ -1,8 +1,4 @@
-#include <cmath>
-#include <cstdio>
-#include <vector>
 #include <iostream>
-#include <algorithm>
 using namespace std;
 
 /** print the sum of all multiples of 3 or 5 strictly less than N and greater than 0
@@ -12,15 +8,14 @@ using namespace std;
 void problem1(int N) {
     // unsigned long int has size of 8 bytes
     // unsigned int has size of 4 bytes and it overflows
-    unsigned long int sum3, sum5, sum15, mul3, mul5, mul15;
+    unsigned long int sum, mul3, mul5, mul15;
     N -= 1;
     mul3 = N / 3;
     mul5 = N / 5;
     mul15 = N / 15;
-    sum3 = (3 * (mul3 + 1) * mul3) / 2;
-    sum5 = (5 * (mul5 + 1) * mul5) / 2;
-    sum15 = (15 * (mul15 + 1) * mul15) / 2;
-    cout << N + 1 << ": " << sum3 + sum5 - sum15 << endl;
+    sum = ((3 * mul3 * mul3 + 3 * mul3) >> 1) + ((5 * mul5 * mul5 + 5 * mul5) >> 1);
+    sum -= (15 * mul15 * mul15 + 15 * mul15) >> 1;
+    cout << N + 1 << ": " << sum << endl;
 }
 
 void test_problem1() {
