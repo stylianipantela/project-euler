a393e0838f20bafd8e591f60c84ab661d03dea78
Stella Pantela <stella.pantela@gmail.com>
2014-10-01 18:17:04 -0400

    problem4

--- problems.cpp
+++ problems.cpp
@@ -58,9 +58,7 @@ void test_problem2() {
 
 /** find largest prime factor of number N
   * @param N < 10^12
-  * @return retuns largest prime factor of N
   */
-
 void problem3(unsigned long long int n) {
     unsigned long long int original_n = n;
     unsigned long long int largest_prime = 2;
@@ -86,10 +84,50 @@ void test_problem3() {
     problem3(600851475143);
 }
 
+/** Get the palindrome from the 3 digit number we pass in
+  * @param n int 3 digit number to be turned into a palindrome
+  * @pre 100 <= @a n < 1000
+  * 
+  * @return retusn n concatenated with reverse(n), example 103 will return 103301
+  */
+int get_palindrome(int n) {
+    string n_str = to_string(n);
+    string n_rev = to_string(n);
+    reverse(n_rev.begin(), n_rev.end());
+    n_str.append(n_rev);
+    return stoi(n_str);
+}
+
+/** find largest prime factor of number N
+  * @param 101101< N <1000000
+  */
+void problem4(int N) {
+    int max = 0;
+
+    for (int i = 999; i >= 100; --i) {
+        max = get_palindrome(i);
+        if (max > N)
+            continue;
+        for (int j = 100; j < 1000; ++j) {
+            if (max % j == 0 && (max / j) / 1000 == 0) {
+                cout << max << endl;
+                return;
+            }
+        }
+    }
+}
+
+void test_problem4() {
+    problem4(101110);
+    problem4(800009);
+}
+
+
 int main() {
     // test_problem1();
     // test_problem2();
-    test_problem3();
-    
+    // test_problem3();
+    test_problem4();
+   
     return 0;
 }
